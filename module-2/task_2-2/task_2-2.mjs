dfbfecf673c94cbc0965dd8430248df666420f88
Anders Moseng <anders.joa.moseng@gmail.com>
2025-09-12 16:04:58 +0200

    Laptop - PC

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -4,27 +4,62 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const x = (2 + 3 * (2 - 4) * 6);
+const y = (2 + 3 * 2 - 4 * 6);
+
+printOut("2 + 3 * (2 - 4) * 6 = " + x);
+printOut("2 + 3 * 2 - 4 * 6 = " + y);
+
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const meters = 25.34;
+const inches = meters / 0.0254;
+printOut(meters + " meters is the same as " + inches.toFixed(2) + " inches");
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const days = 3;
+const hours = 12;
+const minutes = 14;
+const seconds = 45;
+
+const totalMinutes = (days * 24 * 60) + (hours * 60) + (minutes) + (seconds / 60);
+
+printOut("3 days, 12 hours, 14 minutes and 45 seconds converted to minutes is " + totalMinutes + " minutes");
+
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const totalMinutes1 = 6322.52;
+
+const days1 = totalMinutes / 60 / 24;
+const hours1 = totalMinutes / 60;
+const minutes1 = totalMinutes;
+const seconds1 = totalMinutes * 60;
+
+printOut(totalMinutes1 + " minutes is the same as " + days1 + " days, " + hours1 + " hours " + minutes1 + " minutes and " + seconds1 + " seconds");
+
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let USD = 54
+let exhangeRate = 8.6
+
+let NOK = USD * 8.6;
+
+printOut(NOK);
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
