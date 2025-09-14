057408aee369fd7d273c96f14e823cf1e1aa5fd4
amos <anders.joa.moseng@gmail.com>
2025-09-14 18:42:33 +0200

    Task 2.2 submission

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -56,34 +56,69 @@ printOut("--- Part 5 -----------------------------------------------------------
 let USD = 54
 let exhangeRate = 8.6
 
-let NOK = USD * 8.6;
+let NOK = Math.round(USD * 8.6);
 
-printOut(NOK);
+printOut(USD + "$ is " + NOK + "kr");
 
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let quote = "There is much between heaven and earth that we do not understand.";
+
+printOut("There are "+ quote.length + " characters in the quote.");
+printOut("The 19th character in the quote is: " +  quote[19]);
+printOut("The 35th character and 8 characters behind in the quote is: " +  quote.substring(35,43));
+printOut("The index at which \"earth\" starts in the quote is: " + quote.indexOf("earth"));
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let a = 5 > 3;
+let b = 7 >= 7;
+let c = "a" > "b";
+let d = "1" > "a";
+let e = "2500" < "abcd";
+let f = "arne" != "thomas";
+let g = 2 == 5;
+let h = "abcd" >= "bcd";
+
+printOut([a, b, c, d, e, f, g, h]);
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let text1 = parseInt("254");
+let text2 = parseInt("57.23");
+let text3 = parseInt("25 kroner");
+
+printOut([text1, text2, text3]);
+
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let r = Math.floor(Math.random() * 360) + 1;
+
+printOut(r);
+
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let days2 = 131;
+
+let weeks = Math.floor(days2 / 7);
+let remDays = days2 % 7;
+
+printOut(days2 + " dager er det samme som " + weeks + " uker, med " + remDays + " dager igjen.");
+
 printOut(newLine);
